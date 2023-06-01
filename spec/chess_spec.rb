df19758d4ed2c37dc056dd6142e463d5e987c55a
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-06-01 10:40:19 -0400

    Set test for populates pawns in ranks 1 and 7

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,32 +1,68 @@
 require_relative '../lib/board.rb'
 require_relative '../lib/moves.rb'
+# 
+# describe Board do
+    # describe '#initialize' do
+        # it 'Contains an bidimensional array o board matrix' do 
+            # board = Board.new
+            # array = board.instance_variable_get(:@cells)
+            # expect(array).to eql(
+                # [['','','','','','','',''],
+                # ['','','','','','','',''],
+                # ['','','','','','','',''],
+                # ['','','','','','','',''],
+                # ['','','','','','','',''],
+                # ['','','','','','','',''],
+                # ['','','','','','','',''],
+                # ['','','','','','','','']
+            # ])
+        # end
+    # end
+# 
+    # describe '#setup_board'
+# end
+# 
+# describe Moves do
+    # include Moves
+    # describe '#in_boundaries?' do
+        # it 'return true if the move is inside the board' do
+            # move = [0,0]
+            # move1 = [7,7]
+            # move2 = [5,5]
+            # expect(in_boundaries?(move)).to be true
+            # expect(in_boundaries?(move1)).to be true
+            # expect(in_boundaries?(move2)).to be true
+        # end
+        # it 'return false if the move is offside the board' do
+            # move = [8,8]
+            # move1 = [-1,4]
+            # move2 = [-2,-5]
+            # expect(in_boundaries?(move)).to be false
+            # expect(in_boundaries?(move1)).to be false
+            # expect(in_boundaries?(move2)).to be false
+        # end
+    # end
+# 
+    # describe '#same_color?' do
+        # it 'return true if the piece in the target has the same color' do
+            # whiteKnight = Knight.new('white', [0,0])
+            # white
+        # end
+    # end
+# 
+# end
 
 describe Board do
-    describe '#initialize' do
-        it 'Contains an bidimensional array o board matrix' do 
+    describe '#setup_board' do
+        it 'Fills rank 1 with white pawns' do
             board = Board.new
-            array = board.instance_variable_get(:@cells)
-            expect(array).to eql(
-                [['','','','','','','',''],
-                ['','','','','','','',''],
-                ['','','','','','','',''],
-                ['','','','','','','',''],
-                ['','','','','','','',''],
-                ['','','','','','','',''],
-                ['','','','','','','',''],
-                ['','','','','','','','']
-            ])
-
-        end
-    end
-end
-
-describe Moves do
-    include Moves
-    describe '#in_boundaries?' do
-        it 'return true if the move is inside the board' do
-            move = [0,0]
-            expect(in_boundaries?(move)).to be true
+            board.setup_board
+            row = []
+            rank = 1.to_s
+            'abcdefgh'.split.each do |chr|
+                row << board.cells[chr+rank].class
+            end
+            expect(row.all?(Piece)).to be true
         end
     end
 end
