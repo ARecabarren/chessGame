ca70600765772db26743cd29a61b667c81110c03
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-06-14 12:34:23 -0400

    FIlter moves in range for Knight

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,82 +1,101 @@
+# require_relative './board.rb'
+
 class Piece
-    attr_accessor :position
-    attr_reader :type, :color
-    def initialize(type, color, position)
-        @type = type
-        @color = color
-        @position = position
-        @directions = get_directions
-        @first_move = true
-        #Need to set @availables_moves
-        #Need to adapt directions acording to @type
-    end
+  attr_accessor :position, :in_range_moves
+  attr_reader :type, :color, :directions
 
-    def get_directions
-        case @type
-        when 'R'
-            [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        when 'N'
-            [
-                [2,1],[2,-1],[-2,1],[-2,-1],
-                [1,2],[1,-2],[-1,2],[-1,-2]
-            ]
-        when 'B'
-            [[1, 1], [1, -1], [-1, -1], [-1, 1]]
-        when 'Q'
-            [[0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
-        when 'K'
-            [
-                [0,1],[1,1],[1,0],[-1,1],[-1,0],
-                [-1,-1],[0,-1],[1,-1]
-            ]
-        when 'P'
-            if @color == :white
-                if @first_move
-                    [[-1, 0], [-2, 0]]
-                else
-                    [[-1,0]]
-                end
-            else
-                if @first_move
-                    [[1, 0], [2, 0]]
-                else
-                    [[1, 0]]
-                end
-            end
-        else
-            return []
-        end
-    end
+  def initialize(type, color, position)
+    @type = type
+    @color = color
+    @position = position
+    @first_move = true
+    set_directions
+    compute_moves
+  end
 
-    def possible_moves
-        moves = []
-        current_x, current_y = @position
-        @directions.each do |direction|
-            delta_x, delta_y = direction
-            new_x = current_x + delta_x
-            new_y = current_y + delta_y
-
-            while valid_move?([new_x, new_y])
-                moves << [new_x, new_y]
-                new_x += delta_x
-                new_x += delta_y
-            end
-        end
-        moves
-    end
-    
-end
-
-
-class Knight
-    attr_reader :color, :possible_moves
-    attr_accessor :position
-    def initialize(color, position)
-        @position = position
-        @color = color
-        @possible_moves = [
-            [2,1],[2,-1],[-2,1],[-2,-1],
-            [1,2],[1,-2],[-1,2],[-1,-2]
+  def set_directions
+    @directions = case @type
+      when :R
+        [[0, 1], [1, 0], [0, -1], [-1, 0]]
+      when :N
+        [
+          [2, 1], [2, -1], [-2, 1], [-2, -1],
+          [1, 2], [1, -2], [-1, 2], [-1, -2]
         ]
+      when :B
+        [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+      when :Q
+        [[0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+      when :K
+        [
+          [0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0],
+          [-1, -1], [0, -1], [1, -1]
+        ]
+      when :P
+        if @color == :white
+          if @first_move
+            [[-1, 0], [-2, 0]]
+          else
+            [[-1, 0]]
+          end
+        else
+          if @first_move
+            [[1, 0], [2, 0]]
+          else
+            [[1, 0]]
+          end
+        end
+      else
+        []
+      end
+  end
+  # knight example
+  def compute_moves
+    @in_range_moves = []
+    current_x, current_y = cell_to_coord(self.position)
+    case type
+    when :R
+      
+    when :N
+      self.directions.each do |direction|
+        xstep, ystep = direction
+        new_x = current_x + xstep
+        new_y = current_y + ystep
+        new_coord = [new_x,new_y]
+        in_range_moves << new_coord if in_boundaries?(new_coord)
+      end
+    when :B
+
+    when :Q
+
+    when :K
+
+    when :P
+      
+    else
+      
     end
+  end
 end
+
+def cell_to_coord(fileRank)
+  row = fileRank[1].to_i - 1
+  col = 0
+  'abcdefgh'.split('').each do |file|
+    if file == fileRank[0]
+      break
+    else
+      col += 1
+    end
+  end
+  return row, col
+end
+
+def in_boundaries?(coord)
+  x, y = coord
+  x.between?(0,7) && y.between?(0,7) 
+end
+
+# whitePawn = Piece.new(:P,:white,'a2')
+whiteKnight = Piece.new(:N, :white, 'b1')
+whiteKnight.in_range_moves # [[2, 2], [2, 0], [1, 3]]
