c86b141b8dbf76ca2abd2940e910b30fcb3e3b8f
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-06-19 10:21:28 -0400

    Reformat and move methods to board

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,101 +1,36 @@
-# require_relative './board.rb'
-
 class Piece
-  attr_accessor :position, :in_range_moves
+  attr_accessor :position, :legal_moves
   attr_reader :type, :color, :directions
 
-  def initialize(type, color, position)
+  DIRECTIONS = {
+    R: [[0, 1], [1, 0], [0, -1], [-1, 0]],
+    N: [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]],
+    B: [[1, 1], [1, -1], [-1, -1], [-1, 1]],
+    Q: [[0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]],
+    K: [[0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]],
+    P: {
+      white: [[1, 0], [2, 0]],
+      black: [[-1, 0], [-2, 0]]
+    }
+  }
+
+  def initialize(type, color, position,board_cells = nil)
     @type = type
     @color = color
     @position = position
     @first_move = true
-    set_directions
-    compute_moves
+    @directions = set_directions
   end
 
   def set_directions
-    @directions = case @type
-      when :R
-        [[0, 1], [1, 0], [0, -1], [-1, 0]]
-      when :N
-        [
-          [2, 1], [2, -1], [-2, 1], [-2, -1],
-          [1, 2], [1, -2], [-1, 2], [-1, -2]
-        ]
-      when :B
-        [[1, 1], [1, -1], [-1, -1], [-1, 1]]
-      when :Q
-        [[0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
-      when :K
-        [
-          [0, 1], [1, 1], [1, 0], [-1, 1], [-1, 0],
-          [-1, -1], [0, -1], [1, -1]
-        ]
-      when :P
-        if @color == :white
-          if @first_move
-            [[-1, 0], [-2, 0]]
-          else
-            [[-1, 0]]
-          end
-        else
-          if @first_move
-            [[1, 0], [2, 0]]
-          else
-            [[1, 0]]
-          end
-        end
-      else
-        []
-      end
-  end
-  # knight example
-  def compute_moves
-    @in_range_moves = []
-    current_x, current_y = cell_to_coord(self.position)
-    case type
-    when :R
-      
-    when :N
-      self.directions.each do |direction|
-        xstep, ystep = direction
-        new_x = current_x + xstep
-        new_y = current_y + ystep
-        new_coord = [new_x,new_y]
-        in_range_moves << new_coord if in_boundaries?(new_coord)
-      end
-    when :B
-
-    when :Q
-
-    when :K
-
-    when :P
-      
-    else
-      
-    end
+    directions = DIRECTIONS[@type]
+    directions = directions[@color] if directions.is_a?(Hash)
+    directions
   end
 end
 
-def cell_to_coord(fileRank)
-  row = fileRank[1].to_i - 1
-  col = 0
-  'abcdefgh'.split('').each do |file|
-    if file == fileRank[0]
-      break
-    else
-      col += 1
-    end
-  end
-  return row, col
-end
 
-def in_boundaries?(coord)
-  x, y = coord
-  x.between?(0,7) && y.between?(0,7) 
-end
 
 # whitePawn = Piece.new(:P,:white,'a2')
-whiteKnight = Piece.new(:N, :white, 'b1')
-whiteKnight.in_range_moves # [[2, 2], [2, 0], [1, 3]]
+# whiteKnight = Piece.new(:N, :white, 'b1')
+# whiteKnight.in_range_moves # [[2, 2], [2, 0], [1, 3]]
